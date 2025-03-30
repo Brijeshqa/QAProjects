2f0ef7f5056cdc9ada2006614d575d614a211d8f
BrijeshYadav <brijeshjnv@gmail.com>
2025-03-30 13:05:01 +0530

    comments in js

---
+++ comment.js
@@ -0,0 +1,17 @@
+#! there are 3 types of command in javascript, 1. Hashback, 2. singleline 3. multiline
+
+// first programme
+
+console.log('Hello World!');
+
+/**
+ * Adds 2 numbers
+ * @param {*} num1 
+ * @param {*} num2 
+ * @returns sum of above numbers
+ */
+
+
+function sum(num1, num2){
+    return num1+num2;
+};
